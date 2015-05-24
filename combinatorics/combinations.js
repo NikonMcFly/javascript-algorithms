289aac9753f23a058ef4fbf4a77b6fd05957ed35
nikonmcfly <nikonmcfly@gmail.com>
2015-05-23 23:40:58 -0500

    new add

--- combinatorics/combinations.js
+++ combinatorics/combinations.js
@@ -6,4 +6,26 @@ from a grouping, such that the order of selection
 does not matter. *order does not matter*
 */
 
-function combinations(arr, k)
+//arr = array(set of items)
+//Num = (side of each combination)
+function combinations(arr, num, start, idx, current) {
+	var res = [];
+
+	 if (idx === num) {
+	 	res.push(current.slice());
+	 	return;
+	 }
+	 for (var i = start; i < arr.length; i += 1){
+	 	current[idx] = arr[i];
+	 	combinations(arr, num, i + 1, idx + 1, current);
+	 }
+	}
+
+	 function combination(arr, num) {
+	 	var res = [];
+	 	combinations(arr, num, 0, 0, []);
+	 	var temp = res;
+
+	 	res = null;
+	 	return temp;
+	 };
